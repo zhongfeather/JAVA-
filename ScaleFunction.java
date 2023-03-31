c8a862a7045d6b9a56844f4087c77ba23450defe
featherzhong <129075220+zhongfeather@users.noreply.github.com>
2023-03-31 11:50:54 +0800

    Add files via upload

---
+++ ScaleFunction.java
@@ -0,0 +1,15 @@
+package com.madou.uml.cantainer;
+
+public interface ScaleFunction {
+    /**
+     * 缩放一步，可以是负数
+     * @param step
+     */
+    public void scale(int step);
+
+    /**
+     * 重置样式为100
+     */
+    public void reset();
+
+}
